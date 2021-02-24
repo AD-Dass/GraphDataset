e76f1fb7daf7a6ca3d218267d5809d09358165e3
Araan <5108521+AD-Dass@users.norepky.github.com>
2021-02-24 21:57:48 +0000

    origin

--- graphify.py
+++ graphify.py
@@ -5,62 +5,62 @@ class Graphiy():
     def __init__(self):
         self.data = []
 
-    #add new element to the data set
-    def newelem(self,element,children): #add parent as a str, children as a list
-        #check if element already exists
+    #add new node to the data set
+    def newelem(self,node,children): #add parent as a str, children as a list
+        #check if node already exists
         counter =0
         for n in self.data:
-            if element == n['parent']:
+            if node == n['parent']:
                 counter = counter+1
         if counter ==0:    
-            self.data.append({'parent':element, 'children':children})
+            self.data.append({'parent':node, 'children':children})
         else:
-            self.newchildren(element, children)
+            self.newchildren(node, children)
 
 
 
 
-    #add children to existing element
-    def newchild(self,element,child): #add single children at time
-        #check if element exists the update
+    #add children to existing node
+    def newchild(self,node,child): #add single children at time
+        #check if node exists the update
         for n in self.data:
-            if n['parent']==element:
+            if n['parent']==node:
                 if child not in n['children']:
                     n['children'].append(child)
                 else:
                     print('child exists')
 
-    def newchildren(self,element,child):
+    def newchildren(self,node,child):
         for n in self.data:
-            if n['parent']==element:
+            if n['parent']==node:
                 for m in child:
                     if m not in n['children']:
                         n['children'].append(m)
                     else:
                         print('This child already exists',m)
 
-    #remove children from a existing element
-    def removechild(self,element,child):
+    #remove children from a existing node
+    def removechild(self,node,child):
         for n in self.data:
-            if n['parent']==element:
+            if n['parent']==node:
                 n['children'].remove(child)
 
-    #name all the children for an element
-    def reportchildren(self,element):
+    #name all the children for an node
+    def reportchildren(self,node):
         for n in self.data:
-            if n['parent'] == element:
+            if n['parent'] == node:
                 print(self.data)
                 print(n['children'])
                 
 
 if __name__=='__main__':
     Mygraph=Graphiy()
-    element ='A'
+    node ='A'
     child = ['B','C','D']
-    Mygraph.newelem(element,child)
-    Mygraph.reportchildren(element)
-    element ='A'
+    Mygraph.newelem(node,child)
+    Mygraph.reportchildren(node)
+    node ='A'
     child = 'D'
-    Mygraph.newchild(element,child)
+    Mygraph.newchild(node,child)
     Mygraph.newelem('B',['D','E'] )
     Mygraph.reportchildren('A')
